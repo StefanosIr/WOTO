2c2e6d9612a3397200104b490be85e945b0d3662
Stefanos Irodotou <ettaros@hotmail.com>
2026-02-14 22:04:00 +0200

    Refactor Controls1: Add constants, better state management, cached references

--- Assets/Scripts/Controls1.cs
+++ Assets/Scripts/Controls1.cs
@@ -1,93 +1,145 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Controls1 : MonoBehaviour {
 	
+	[Header("Movement Settings")]
+	public float rotationSpeed = 100.0f;
+	
+	[Header("Animation Timings")]
+	public float jumpAnimationDuration = 0.1f;
+	public float punchAnimationDuration = 0.5f;
+	public float kickAnimationDuration = 0.5f;
+	
 	private Animator myAnimator;
+	
+	// Animation parameter name constants
+	private const string ANIM_VSPEED = "VSpeed";
+	private const string ANIM_HSPEED = "HSpeed";
+	private const string ANIM_JUMPING = "Jumping";
+	private const string ANIM_PUNCHING = "Punching";
+	private const string ANIM_KICKING = "Kicking";
+	private const string ANIM_TURNING_LEFT = "TurningLeft";
+	private const string ANIM_TURNING_RIGHT = "TurningRight";
+	private const string ANIM_CURRENT_ACTION = "CurrentAction";
 	
 	// Use this for initialization
 	void Start () {
 		myAnimator = GetComponent<Animator>();
+		if (myAnimator == null) {
+			Debug.LogError("Animator component not found on " + gameObject.name);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		myAnimator.SetFloat ("VSpeed", Input.GetAxis ("Vertical"));
-		myAnimator.SetFloat ("HSpeed", Input.GetAxis ("Horizontal"));
+		if (myAnimator == null) return;
 		
-		if(Input.GetButtonDown ("Jump")){
-			myAnimator.SetBool ("Jumping", true);
-			Invoke ("StopJumping", 0.1f); //calls the invoke after a specified time
-		}
-
-		if(Input.GetButtonDown ("Fire1")){
-			myAnimator.SetBool ("Punching", true);
-			Invoke ("StopPunching", 0.5f); //calls the invoke after a specified time
-		}
-
-		if(Input.GetButtonDown ("Fire2")){
-			myAnimator.SetBool ("Kicking", true);
-			Invoke ("StopKicking", 0.5f); //calls the invoke after a specified time
-		}
-		
-		
-		if(Input.GetKey("q")){
-			transform.Rotate (Vector3.down * Time.deltaTime * 100.0f);
-			if((Input.GetAxis ("Vertical") == 0f) && (Input.GetAxis ("Horizontal") == 0)){ //check if moving
-				myAnimator.SetBool ("TurningLeft", true);
-			}
-			
-		} else {
-			myAnimator.SetBool ("TurningLeft", false);
-		}
-		
-		
-		if(Input.GetKey("e")){
-			transform.Rotate (Vector3.down * Time.deltaTime * -100.0f);
-			if((Input.GetAxis ("Vertical") == 0f) && (Input.GetAxis ("Horizontal") == 0)){
-				myAnimator.SetBool ("TurningRight", true);
-			}
-		} else {
-			myAnimator.SetBool ("TurningRight", false);
-		}
-		
-		if(Input.GetKeyDown ("1")){
-			if(myAnimator.GetInteger("CurrentAction") == 0){
-				myAnimator.SetInteger("CurrentAction", 1);				
-			} else if (myAnimator.GetInteger ("CurrentAction") == 1){
-				myAnimator.SetInteger ("CurrentAction", 0);
-			}
-		}
-		
-		if(Input.GetKeyDown ("2")){
-			if(myAnimator.GetInteger ("CurrentAction") == 0){
-				myAnimator.SetInteger ("CurrentAction", 2);				
-			} else if (myAnimator.GetInteger ("CurrentAction") == 2){
-				myAnimator.SetInteger ("CurrentAction", 0);
-			}
-		}
-		
-		if(Input.GetKeyDown ("3")){
-			myAnimator.SetLayerWeight (1, 1f);
-			myAnimator.SetInteger("CurrentAction", 3);
-		}
-		
-		if(Input.GetKeyUp ("3")){
-			myAnimator.SetInteger ("CurrentAction", 0);
-		}
-		
+		HandleMovement();
+		HandleCombat();
+		HandleRotation();
+		HandleActions();
 	}
 	
-	void StopJumping(){
-		myAnimator.SetBool ("Jumping", false);
+	void HandleMovement() {
+		// Update movement animation parameters
+		float verticalInput = Input.GetAxis("Vertical");
+		float horizontalInput = Input.GetAxis("Horizontal");
+		
+		myAnimator.SetFloat(ANIM_VSPEED, verticalInput);
+		myAnimator.SetFloat(ANIM_HSPEED, horizontalInput);
+		
+		// Handle jumping
+		if (Input.GetButtonDown("Jump")) {
+			myAnimator.SetBool(ANIM_JUMPING, true);
+			Invoke("StopJumping", jumpAnimationDuration);
+		}
 	}
-
-		void StopPunching(){
-			myAnimator.SetBool ("Punching", false);
+	
+	void HandleCombat() {
+		// Handle punching
+		if (Input.GetButtonDown("Fire1")) {
+			myAnimator.SetBool(ANIM_PUNCHING, true);
+			Invoke("StopPunching", punchAnimationDuration);
+		}
+		
+		// Handle kicking
+		if (Input.GetButtonDown("Fire2")) {
+			myAnimator.SetBool(ANIM_KICKING, true);
+			Invoke("StopKicking", kickAnimationDuration);
+		}
 	}
-
-	void StopKicking(){
-		myAnimator.SetBool ("Kicking", false);
+	
+	void HandleRotation() {
+		float verticalInput = Input.GetAxis("Vertical");
+		float horizontalInput = Input.GetAxis("Horizontal");
+		bool isMoving = (verticalInput != 0f) || (horizontalInput != 0f);
+		
+		// Turn left (Q key)
+		if (Input.GetKey(KeyCode.Q)) {
+			transform.Rotate(Vector3.down * Time.deltaTime * rotationSpeed);
+			myAnimator.SetBool(ANIM_TURNING_LEFT, !isMoving);
+		} else {
+			myAnimator.SetBool(ANIM_TURNING_LEFT, false);
+		}
+		
+		// Turn right (E key)
+		if (Input.GetKey(KeyCode.E)) {
+			transform.Rotate(Vector3.down * Time.deltaTime * -rotationSpeed);
+			myAnimator.SetBool(ANIM_TURNING_RIGHT, !isMoving);
+		} else {
+			myAnimator.SetBool(ANIM_TURNING_RIGHT, false);
+		}
 	}
-
+	
+	void HandleActions() {
+		int currentAction = myAnimator.GetInteger(ANIM_CURRENT_ACTION);
+		
+		// Action 1 (toggle)
+		if (Input.GetKeyDown(KeyCode.Alpha1)) {
+			ToggleAction(currentAction, 1);
+		}
+		
+		// Action 2 (toggle)
+		if (Input.GetKeyDown(KeyCode.Alpha2)) {
+			ToggleAction(currentAction, 2);
+		}
+		
+		// Action 3 (hold)
+		if (Input.GetKeyDown(KeyCode.Alpha3)) {
+			myAnimator.SetLayerWeight(1, 1f);
+			myAnimator.SetInteger(ANIM_CURRENT_ACTION, 3);
+		}
+		
+		if (Input.GetKeyUp(KeyCode.Alpha3)) {
+			myAnimator.SetInteger(ANIM_CURRENT_ACTION, 0);
+		}
+	}
+	
+	void ToggleAction(int currentAction, int actionNumber) {
+		if (currentAction == actionNumber) {
+			myAnimator.SetInteger(ANIM_CURRENT_ACTION, 0);
+		} else {
+			myAnimator.SetInteger(ANIM_CURRENT_ACTION, actionNumber);
+		}
+	}
+	
+	// Animation state reset methods
+	void StopJumping() {
+		if (myAnimator != null) {
+			myAnimator.SetBool(ANIM_JUMPING, false);
+		}
+	}
+	
+	void StopPunching() {
+		if (myAnimator != null) {
+			myAnimator.SetBool(ANIM_PUNCHING, false);
+		}
+	}
+	
+	void StopKicking() {
+		if (myAnimator != null) {
+			myAnimator.SetBool(ANIM_KICKING, false);
+		}
+	}
 }
