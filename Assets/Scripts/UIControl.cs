9db8605f9043e63218370e3032891c027307157f
Stefanos Irodotou <ettaros@hotmail.com>
2026-02-14 22:03:13 +0200

    Modernize UIControl: Replace deprecated LoadLevel with SceneManager

--- Assets/Scripts/UIControl.cs
+++ Assets/Scripts/UIControl.cs
@@ -1,14 +1,34 @@
-﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class UIControl : MonoBehaviour {
 
+	/// <summary>
+	/// Changes to a different scene by name
+	/// </summary>
+	/// <param name="sceneName">Name of the scene to load</param>
 	public void ChangeScene(string sceneName)
 	{
-		Application.LoadLevel (sceneName);
+		SceneManager.LoadScene(sceneName);
 	}
 
+	/// <summary>
+	/// Quits the application
+	/// </summary>
+	public void QuitGame()
+	{
+		Application.Quit();
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#endif
+	}
 
-
-
+	/// <summary>
+	/// Reloads the current scene
+	/// </summary>
+	public void RestartScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
 }
